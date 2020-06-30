51f14477be8530ae339d6a06a5e3ad759bfb4fd1
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-06-30 22:57:16 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+package calculation;
+public class Calculator{
+	public void add(int a, int b)
+	{
+		System.out.println("Addition is "+(a+b));
+	}
+	public void sub(int a, int b)
+	{
+		System.out.println("Subtraction is "+(a-b));
+	}
+	public void div(int a, int b)
+	{
+		System.out.println("Division  is "+(a/b));
+	}
+	public void mul(int a, int b)
+	{
+		System.out.println("Muliplication is "+(a*b));
+	}
+}
+	
