65051a0d957f3f1dc6f71017f3e8499fa17059d7
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-06-30 22:55:08 +0530

    Add files via upload

---
+++ P_Calculator.java
@@ -0,0 +1,11 @@
+class P_Calculator{
+	public static void main(String arg[])
+	{
+		calculation.Calculator c= new calculation.Calculator();
+		int a=65, b=5;
+		c.add(a,b);
+		c.sub(a,b);
+		c.div(a,b);
+		c.mul(a,b);
+	}
+}
