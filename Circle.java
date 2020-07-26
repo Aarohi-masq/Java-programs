8e6dcfbf02844597414bbe20bbceb23fd2429590
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-07-26 23:28:17 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Circle
+{
+public static void main(String args[])
+{
+double ar,cir;
+Scanner scan= new Scanner(System.in);
+int r=scan.nextInt();
+ar=3.14*r*r;
+cir=2*3.14*r;
+System.out.println(ar); 
+System.out.println(cir);
+}
+}
