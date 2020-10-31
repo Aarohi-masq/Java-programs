ee84fb64946a3474ce1f7e011cc51ccb6f20e426
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-10-31 22:22:29 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,20 @@
+public class Q1
+{
+	public static void main(String a[])
+    {
+		String st1="Aarohi";
+		String st2="Mangal";
+		String st="aarohi";
+		String st3= st1.concat(st2);
+		System.out.println(st3);
+		System.out.println(st1.equalsIgnoreCase(st));
+		System.out.println(st.toUpperCase());
+		System.out.println(st1.compareTo(st));
+		System.out.println(st.charAt(4));
+		Boolean b1 = true;  
+        Boolean b2 = false; 
+        System.out.println(b1.equals(b2));		
+		
+	}
+}
+	
