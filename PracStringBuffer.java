b4c3888bdd8acb2db0cd42c2d6a967037c7907c8
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-10-31 22:24:14 +0530

    Add files via upload

---
+++ PracStringBuffer.java
@@ -0,0 +1,10 @@
+class PracStringBuffer{
+	public static void main(String args[]){
+		/*StringBuffer sb=new StringBuffer("Hello");
+		sb.append("Java");
+		System.out.println(sb);*/
+		StringBuffer sb =new StringBuffer("Hello");
+		sb.insert(1,"aarohi");
+		System.out.println(sb);
+	}
+}
